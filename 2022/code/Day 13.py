36d4d7092f8b4302b9fe02a1aa3b202649312455
Alex <alexandra.richards@lshtm.ac.uk>
2022-12-13 22:08:56 +0000

    learning python for day 13

---
+++ 2022/code/Day 13.py
@@ -0,0 +1,86 @@
+import json 
+
+def sign(a):
+    if a < 0:
+        return -1
+    elif a == 0:
+        return 0
+    elif a > 0:
+        return 1
+
+
+def comp(p):
+    l,r = p
+    i = 0
+    
+    ll = len(l)
+    lr = len(r)
+    
+    if ll == 0 or lr == 0:
+        return sign(lr-ll) >= 0
+    
+    # Init: i = 0, l[0..0) == [] == r[0..)
+    while i < min(ll,lr) and compare(l[i],r[i]) == 0: # I: l[0..i) == r[0..i)
+        i += 1
+    
+    # Post: l[0..i) == r[0..i) 
+    # AND i == min(ll,lr) or compare(l[i],r[i]) != 0
+    
+    
+    if i == min(ll,lr):
+        return sign(lr-ll) >= 0
+    elif compare(l[i],r[i]) in [1, -1]: # i < min(ll, lr)
+        return compare(l[i],r[i]) == 1
+    else:
+        error
+    
+
+def compare(l,r):
+    if l == r:
+        return 0
+    if isinstance(l, int) and isinstance(r, int):
+        if l > r:
+            return -1
+        else:
+            return 1
+    elif isinstance(l, list) and isinstance(r, int):
+        return compare(l,[r])
+    elif isinstance(l, int) and isinstance(r, list):
+        return compare([l],r)
+    elif isinstance(l, list) and isinstance(r, list):
+        if len(l) == 0 and len(r) > 0:
+            return 1
+        elif len(l) > 0 and len(r) == 0:
+            return -1
+        elif len(l) == 0 and len(r) == 0:
+            return 0
+        
+        l_head = l[0]
+        r_head = r[0]
+        out = compare(l_head, r_head)
+        if out == 0:
+            return compare(l[1:],r[1:])
+        else:
+            return out
+
+
+fname = "/Users/lsh1700322/Documents/GitHub/Advent-of-Code/2022/data/data 13.json"
+f = open(fname)
+data = json.load(f)
+pairs = data
+
+c = 1
+s = 0
+for pair in pairs:
+    l, r = pair
+    print("l: {0} \n r: {1}".format(l,r))
+    if r == None:
+        break
+    if comp(pair):
+        
+        s += c
+    c += 1
+
+print(s)
+        
+        
